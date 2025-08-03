8227bf3776fd6a38e288429b89a664e3a5c28c97
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-08-03 17:48:28 +0900

    v0.3 -> v0.4

--- tests/test_station.py
+++ tests/test_station.py
@@ -8,10 +8,10 @@ from tagoapi.utils.cache import Cache
 
 
 
-def test_cache():
-    cache = Cache("caches/station.pkl")
-    station_list = cache.get("stations_2025_06_15.csv")
-    assert isinstance(station_list, list)
+# def test_cache():
+#     cache = Cache("caches/station.pkl")
+#     station_list = cache.get("stations_2025_06_15.csv")
+#     assert isinstance(station_list, list)
 
 def test_statin():
     assert isinstance(get_station("민들"), list)
