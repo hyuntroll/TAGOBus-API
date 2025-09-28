67c48d3237bae02fc798c1ab9a90d1634a31839b
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-09-28 18:09:23 +0900

    feat: create BaseList extends list

---
+++ tagoapi/models/BaseList.py
@@ -0,0 +1,13 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from tagoapi.models.BaseModel import BaseModel
+    from tagoapi import TAGOClient
+
+class BaseList(list): # list 상속 받아도 좋을듯
+
+    def as_list(self) -> list["BaseModel"]:
+        return list(self)
+
+    def set_client(self, client: "TAGOClient"):
+        for element in self:
+            element.set_client(client)
--- tagoapi/models/BaseModel.py
+++ tagoapi/models/BaseModel.py
@@ -1,22 +1,31 @@
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from tagoapi import TAGOClient
+    from tagoapi.models.BaseList import BaseList
 
 class BaseModel:
     cache_key = "BaseModel:<id>"
-    def __init__(self, client: "TAGOClient"):
-        self._client = client
+    def __init__(self):
+        self._client = None
           
     def to_dict(self) -> dict: ...
 
     def to_dict(self):
         return vars(self)
 
-    @classmethod
-    def from_dict(cls, data: dict, client: "TAGOClient") -> "BaseModel": ...
+    def set_client(self, client: "TAGOClient"):
+        self._client = client
 
     @classmethod
-    def from_list(cls, data: list, client: "TAGOClient") -> list["BaseModel"]:
-        return [cls.from_dict(v, client) for v in data]
+    def from_dict(cls, data: dict) -> "BaseModel": ...
+
+    @classmethod
+    def from_list(cls, data: list) -> "BaseList":
+        model = BaseList()
+        for element in data:
+            model.append(cls.from_dict(element))
+
+        return model
+        # return [cls.from_dict(v) for v in data]
     
 
