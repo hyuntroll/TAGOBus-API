a892022c9cb07c3fd7b0db839352ee71c01a4172
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-11-12 23:38:39 +0900

    chore: auth 삭제 및 build_params 변경

--- tagoapi/utils/params.py
+++ tagoapi/utils/params.py
@@ -1,25 +1,24 @@
-from typing import TYPE_CHECKING
-from typing import TypeVar
-
-U = TypeVar('U', dict[str], list)  # list, dict으로 반환할 때
-
-
-if TYPE_CHECKING:
-    from tagoapi import TAGOAuth
-
 def build_params(
-        auth: "TAGOAuth",
-        numOfRows: int = 300,
-        pageNo: int = 1,
-        **kwargs: dict
-    ) -> dict:
+        service_key: str,
+        num_of_rows: int = 300,
+        page_no: int = 1,
+        kwargs: dict = None
+) -> dict:
+    #TODO: 설명 수정
+    """
+    요청을 보내기 위한 파라미터를 완성합니다.
+    :param service_key: 서비스 키
+    :param num_of_rows: 항목당 나올 횟수 설정
+    :param page_no: 페이지
+    :param kwargs: 안에 들어가야하는 파라미터
+    """
 
     return {
-            "serviceKey": auth.serviceKey,
-            "numOfRows": numOfRows,
-            "pageNo": pageNo,
+            "serviceKey": service_key,
+            "numOfRows": num_of_rows,
+            "pageNo": page_no,
             "_type": "json",
-            **{key: value for key, value in kwargs.items() if value}
+            **kwargs,
         }
 
 def parse_metadata(res: dict) -> U | None:
