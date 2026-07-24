892e093d1dba7b8f926be6c59c551d017f3d40ec
hyuntroll <hsm200905292@gmail.com>
2026-07-24 10:20:25 +0900

    refactor: 불필요한 __init__ 코드 삭제

--- src/tagoapi/resources/vehicle_resource.py
+++ src/tagoapi/resources/vehicle_resource.py
@@ -1,7 +1,5 @@
-from tagoapi._internal.transport import HttpTransport
 from .resource import TagoResource
 
 
-class ArrivalResource(TagoResource):
-    def __init__(self, transport: HttpTransport) -> None:
-        super().__init__(transport)
+class VehicleResource(TagoResource):
+    middle_path = "/BusLcInfoInqireService"
